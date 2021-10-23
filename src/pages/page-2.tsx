0b5cca97ae107ed7ed91360cbc6403f9d6868de7
ztaaso <ztaaso.cedaspy@gmail.com>
2021-10-23 05:12:51 -0300

    initial commit

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -4,11 +4,11 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
+const SecondPage: React.FC = () => (
   <Layout>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <p>Welcome to page 22</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
