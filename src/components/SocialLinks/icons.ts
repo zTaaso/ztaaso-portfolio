b0e2888b5c9a2ce965578fa747945151bd379436
ztaaso <ztaaso.cedaspy@gmail.com>
2022-02-13 15:58:20 -0300

    feat: styled-icons added

---
+++ src/components/SocialLinks/icons.ts
@@ -0,0 +1,13 @@
+import { GitHub } from 'styled-icons/boxicons-logos/GitHub';
+import { Youtube } from 'styled-icons/boxicons-logos/Youtube';
+import { Instagram } from 'styled-icons/boxicons-logos/Instagram';
+import { Linkedin } from 'styled-icons/boxicons-logos/Linkedin';
+
+const Icons = {
+  GitHub,
+  Youtube,
+  Instagram,
+  Linkedin,
+};
+
+export default Icons;
